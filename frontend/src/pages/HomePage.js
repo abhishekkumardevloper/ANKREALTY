69b66ec497344a511b47d0ac4d854860c254b901
abhishekkumardevloper <abhishekkumar.devloper@gmail.com>
2026-04-19 09:36:24 +0530

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,4 +1,3 @@
-// src/pages/HomePage.jsx
 import React, { useMemo, useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
